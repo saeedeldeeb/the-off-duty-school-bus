495570afd9ff2b7c6f81b4bb8a82f31f677e38ed
eldeeb <saeedeldeeb1@gmail.com>
2024-05-11 15:49:57 +0300

    Added Vehicle Id to Trips table

--- BusManagement.Core.Data/Trip.cs
+++ BusManagement.Core.Data/Trip.cs
@@ -9,6 +9,8 @@ public class Trip : IEntity<Guid>, ICreatingTimeStamp, IUpdatingTimeStamp
     public string Name { get; set; }
     public Point StartPoint { get; set; }
     public Point EndPoint { get; set; }
+    public Guid? VehicleId { get; set; }
+    public Vehicle? Vehicle { get; set; }
     public DateTime CreationDateTime { get; set; }
     public DateTime? ModificationDateTime { get; set; }
 }
